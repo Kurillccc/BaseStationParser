062a8109b583c1a828f163af9e513703577f5a99
Kurillccc <89049125360@mail.ru>
2025-05-06 20:41:02 +0300

    delta added

---
+++ app/delta.py
@@ -0,0 +1,40 @@
+import json
+
+def generate_delta(config_path, patched_path, delta_path):
+    with open(config_path, 'r') as f:
+        config = json.load(f)
+    with open(patched_path, 'r') as f:
+        patched = json.load(f)
+
+    additions = []
+    deletions = []
+    updates = []
+
+    config_keys = set(config.keys())
+    patched_keys = set(patched.keys())
+
+    # additions - добавили в новую патч версию
+    for key in patched_keys - config_keys:
+        additions.append({"key": key, "value": patched[key]})
+
+    # deletions - убрали из старой версии
+    for key in config_keys - patched_keys:
+        deletions.append(key)
+
+    # updates - разница между новой и старой версией (измененные данные)
+    for key in config_keys & patched_keys:
+        if config[key] != patched[key]:
+            updates.append({
+                "key": key,
+                "from": config[key],
+                "to": patched[key]
+            })
+
+    delta = {
+        "additions": additions,
+        "deletions": deletions,
+        "updates": updates
+    }
+
+    with open("output/" + delta_path, 'w') as f:
+        json.dump(delta, f, indent=4)
--- run.py
+++ run.py
@@ -1,15 +1,22 @@
 from app.bulder import build_tree, build_meta
 from app.inputParser import parse_input
 from app.writer import write_output_xml, write_output_json
+from app.delta import generate_delta
 
 if __name__ == "__main__":
     input_file = "input/impulse_test_input.xml"
+    config = "input/config.json"
+    patched_config = "input/patched_config.json"
     xml_output = "config.xml"
     json_output = "meta.json"
+    delta = "delta.json"
 
     classes, attributes, aggregations = parse_input(input_file)
+
     xml_tree = build_tree("BTS", attributes, aggregations)
     write_output_xml(xml_tree, xml_output)
 
     meta = build_meta(classes, attributes, aggregations)
     write_output_json(meta, json_output)
+
+    generate_delta(config, patched_config, delta)
